67d17fd71e83c55a810064ff094a738d6aa0e48f
Zack Kaplan <zmka2021@mymail.pomona.edu>
2023-11-15 15:42:35 -0800

    morechanges

--- chest.java
+++ chest.java
@@ -1,8 +1,12 @@
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class chest {
 
@@ -10,30 +14,71 @@ public class chest {
         createAndShowGUI();
     }
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Grid Layout Example");
+        JFrame frame = new JFrame("Chest Day");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(6, 2)); // 6 rows, 2 columns
+        panel.setLayout(new GridLayout(4, 5)); // 4 rows, 5 columns
+
+        // Add column titles
+        panel.add(new JLabel("Exercises"));
+        panel.add(new JLabel("Sets"));
+        panel.add(new JLabel("Reps"));
+        panel.add(new JLabel("Weight"));
+        panel.add(new JLabel("Update?"));
 
         // Add components for each row
-        panel.add(new JLabel("Exercises"));
-        panel.add(new JButton("Edit"));
-        panel.add(new JLabel("x"));
-        panel.add(new JButton("Edit"));
-        panel.add(new JLabel("y"));
-        panel.add(new JButton("Edit"));
-        panel.add(new JLabel("z"));
-        panel.add(new JButton("Edit"));
-        panel.add(new JLabel("w"));
-        panel.add(new JButton("Edit"));
-        panel.add(new JLabel("q"));
-        panel.add(new JButton("Edit"));
+        addRow(panel, "Incline DB Press");
+        addRow(panel, "Machine Press");
+        addRow(panel, "Decline Cable Press");
 
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
     }
+
+    // Helper method to add a row with labels and buttons
+    private static void addRow(JPanel panel, String title) {
+
+        panel.add(new JLabel(title));
+
+        JTextField textField1 = new JTextField();
+        panel.add(textField1);
+        textField1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JLabel userInputLabel = new JLabel(textField1.getText());
+                panel.remove(textField1); // Remove the text field from the panel
+                panel.add(userInputLabel); // Add the label with user input to the panel
+                panel.revalidate(); // Revalidat
+            }
+        });
+
+
+        JTextField textField2 = new JTextField();
+        panel.add(textField2);
+        textField2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                JLabel userInputLabel = new JLabel(textField2.getText());
+                panel.remove(textField2);
+                panel.add(userInputLabel);
+                panel.revalidate();
+            }
+        });
+
+        JTextField textField3 = new JTextField();
+        panel.add(textField3);
+        textField3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                JLabel userInputLabel = new JLabel(textField3.getText());
+                panel.remove(textField3);
+                panel.add(userInputLabel);
+                panel.revalidate();
+            }
+        });
+
+        panel.add(new JButton("Edit"));
+    }
+
 
     public static void main(String[] args) {
         new chest();
