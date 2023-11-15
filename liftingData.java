277ccce61c1a2ad3a6a603f31cdc21058596221c
Zack Kaplan <zmka2021@mymail.pomona.edu>
2023-11-15 14:08:15 -0800

    addeduserfunc

--- liftingData.java
+++ liftingData.java
@@ -3,32 +3,42 @@
  */
 
  import javax.swing.*;
- import java.awt.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import java.awt.*;
+ import java.awt.event.*;
 
  public class liftingData{
 
     private JFrame f; 
     private JPanel p;
     private JButton b1;
-    private JLabel lab;
+    private JButton b2;
+
+    private GridBagConstraints c;
 
     public liftingData(){
         gui();
     }
 
     public void gui(){
-        f = new JFrame("Creativity tuts");
+        f = new JFrame("Lifting app Project");
         f.setVisible(true);
         f.setSize(600, 400);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         p = new JPanel(new GridBagLayout());
-        p.setBackground(Color.BLUE);
+        p.setBackground(Color.LIGHT_GRAY);
 
         b1 = new JButton("Start lifting");
         b2 = new JButton("Schedule");
+        
+        b1.addActionListener(new buttonListener1());
+        
+        b2.addActionListener(new buttonListener2());
 
-        GridBagConstraints c = new GridBagConstraints();
+        c = new GridBagConstraints();
 
         c.insets = new Insets(10,10,10,10);
         c.gridx = 0;
@@ -41,6 +51,83 @@
         f.add(p);
 
     }
+
+    private class buttonListener1 implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            f.setVisible(false);
+            // Create and display a new JFrame with a textbox for user input
+            JFrame newFrame = new JFrame("New Frame with Textbox");
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+            JTextField textField = new JTextField(20);
+            panel.add(textField, BorderLayout.NORTH);
+            newFrame.add(panel);
+
+            newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            newFrame.setSize(600, 400);
+            newFrame.setVisible(true);
+
+            textField.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e){
+                    checkInput(textField, newFrame);
+                }
+
+                @Override 
+                public void removeUpdate(DocumentEvent e){
+                    checkInput(textField, newFrame);
+                }
+                @Override 
+                public void changedUpdate(DocumentEvent e){
+                    checkInput(textField, newFrame);
+                }
+            });
+        }
+    }
+
+    private class buttonListener2 implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            f.setVisible(false);
+            // Create and display a new JFrame with a textbox for user input
+            JFrame newFrame = new JFrame("New Frame with Textbox");
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+            JTextField textField = new JTextField(20);
+            panel.add(textField, BorderLayout.NORTH);
+            newFrame.add(panel);
+
+            newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            newFrame.setSize(600, 400);
+            newFrame.setVisible(true);
+
+            textField.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e){
+                    checkInput(textField, newFrame);
+                }
+
+                @Override 
+                public void removeUpdate(DocumentEvent e){
+                    checkInput(textField, newFrame);
+                }
+                @Override 
+                public void changedUpdate(DocumentEvent e){
+                    checkInput(textField, newFrame);
+                }
+            });
+        }
+    }
+
+    private void checkInput(JTextField textField, JFrame frame){
+        String userInput = textField.getText();
+
+        if(userInput.equals("exit")){
+            frame.setVisible(false);
+            f.setVisible(true);
+        }
+
+    }
+
 
     public static void main(String[] args){
 
